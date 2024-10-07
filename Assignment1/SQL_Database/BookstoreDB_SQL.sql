12bb1628495df3400a816b7761f09b2ffb347278
DanhNguyennene <danhvm12345@gmail.com>
2024-10-07 14:59:55 +0700

    change date to datetime + add manipulation file

--- Assignment1/SQL_Database/BookstoreDB_SQL.sql
+++ Assignment1/SQL_Database/BookstoreDB_SQL.sql
@@ -2,98 +2,109 @@ DROP DATABASE IF EXISTS EBookstoreDB;
 CREATE DATABASE EBookstoreDB;
 
 USE EBookstoreDB;
-
 CREATE TABLE USER (
-    username VARCHAR(50) PRIMARY KEY,
-    name VARCHAR(100),
-    phone_number VARCHAR(20),
-    email VARCHAR(100),
-    password VARCHAR(50)
+		Username VARCHAR(50) PRIMARY KEY,
+        Name varchar(100),
+        Phone_Number varchar(20),
+        Email varchar(100),
+		Passwords varchar(50)
 );
 
-CREATE TABLE CUSTOMER (
-    username VARCHAR(50) PRIMARY KEY,
-    address VARCHAR(255),
-    bank_acc VARCHAR(50),
+create table CUSTOMER(
+	Username varchar(50) primary key,
+    address varchar(255),
+    bank_account varchar(50),
+    foreign key (Username) references USER(Username)
+);
+
+create table EMPLOYEE (
+	Username VARCHAR(50) PRIMARY KEY,
+    Employee_Id VARCHAR(20),
     FOREIGN KEY (username) REFERENCES USER(username)
 );
 
-CREATE TABLE EMPLOYEE (
-    username VARCHAR(50) PRIMARY KEY,
-    employee_id INT,
-    FOREIGN KEY (username) REFERENCES USER(username)
-);
-
-CREATE TABLE AUTHOR (
-    author_id INT PRIMARY KEY,
+create table AUTHOR(
+	Author_Id INT PRIMARY KEY,
     name VARCHAR(100),
-    dob DATE,
+    Dob DATE,
     biography TEXT
 );
 
-CREATE TABLE PUBLISHER (
-    pu_id INT PRIMARY KEY,
-    pu_name VARCHAR(100),
-    pu_phone_number VARCHAR(50),
-    pu_address VARCHAR(255)
+create table PUBLISHER(
+	Publisher_Id INT primary key,
+    Name VARCHAR(100),
+    Phone_Number VARCHAR(50),
+    Address VARCHAR(255)
 );
 
-CREATE TABLE BOOK (
-    book_id INT PRIMARY KEY,
-    title VARCHAR(255),
-    price DECIMAL(10,2), 
-    author_id INT,
-    pu_id INT,
-    FOREIGN KEY (author_id) REFERENCES AUTHOR(author_id),
-    FOREIGN KEY (pu_id) REFERENCES PUBLISHER(pu_id)
+create table BOOK(
+	Book_Id INT primary key,
+    Title VARCHAR(255),
+    Price DECIMAL(10,2), 
+	Author_Id INT,
+    Publisher_Id INT,
+    foreign key (Author_Id) References AUTHOR(Author_Id),
+    foreign key (Publisher_Id) references PUBLISHER(Publisher_Id)
 );
 
-CREATE TABLE GENRE (
-    gen_id INT PRIMARY KEY,
+create table GENRE(
+	Genre_Id INT primary key,
     genre_name VARCHAR(100),
-    description TEXT
+    Description TEXT
 );
 
-CREATE TABLE BOOK_GENRE (
-    book_id INT,
-    gen_id INT,
-    PRIMARY KEY (book_id, gen_id),
-    FOREIGN KEY (book_id) REFERENCES BOOK(book_id),
-    FOREIGN KEY (gen_id) REFERENCES GENRE(gen_id)
+create table BOOK_GENRE(
+	Book_Id int,
+    Genre_Id int,
+    primary key (Book_Id, Genre_Id),
+    foreign key (Book_Id) references BOOK(Book_Id),
+    foreign key (Genre_Id) references GENRE(Genre_Id)
 );
 
-CREATE TABLE `ORDER` (
-    order_id INT PRIMARY KEY,
-    order_time DATETIME,
-    order_status ENUM('Pending', 'Processing', 'Completed', 'Cancelled', 'Failed'),
-    username VARCHAR(50),
-    FOREIGN KEY (username) REFERENCES CUSTOMER(username)
+create table CUSTOMER_ORDER(
+	Order_Id int primary key,
+    Username VARCHAR(50),
+    foreign key (Username) references CUSTOMER(Username)
 );
 
-CREATE TABLE ORDER_BOOK (
-    order_id INT,
-    book_id INT,
-    quantity INT,
-    PRIMARY KEY (order_id, book_id),
-    FOREIGN KEY (order_id) REFERENCES `ORDER`(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOK(book_id)
+create table ORDER_BOOK(
+	Order_Id int,
+    Book_Id int,
+    Order_Status ENUM('Success', 'Pending' , 'Failed'), -- Dieu chinh sau --
+    Order_Time DATETIME,
+    Quantity int,
+    Primary key (Order_Id, Book_Id),
+	foreign key (Order_Id) references CUSTOMER_ORDER(Order_Id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+    foreign key (Book_Id) references BOOK(Book_Id)
 );
 
-CREATE TABLE ORDER_PUBLISHER (
-    pu_order_id INT PRIMARY KEY,
-    pu_order_status ENUM('Success', 'Pending', 'Failed'),
-    pu_order_time DATE,
-    username VARCHAR(50),
-    pu_id INT,
-    FOREIGN KEY (username) REFERENCES EMPLOYEE(username),
-    FOREIGN KEY (pu_id) REFERENCES PUBLISHER(pu_id)
+create table ORDER_PUBLISHER(
+	Order_Publisher_Id  int primary key,
+    Order_Publisher_Status ENUM('Success', 'Pending' , 'Failed'), -- Dieu chinh sau --
+    Order_Publisher_Time DATETIME,
+    Username VARCHAR(50),
+    Publisher_Id int,
+	FOREIGN KEY (Username) REFERENCES EMPLOYEE(Username),
+    FOREIGN KEY (Publisher_Id) REFERENCES PUBLISHER(Publisher_Id)
 );
+
 
 CREATE TABLE ORDER_PUBLISHER_BOOK (
-    book_id INT,
-    pu_order_id INT,
-    quantity INT,
-    PRIMARY KEY (book_id, pu_order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOK(book_id),
-    FOREIGN KEY (pu_order_id) REFERENCES ORDER_PUBLISHER(pu_order_id)
+    Order_Publisher_Id INT,
+    Book_Id INT,
+    Quantity INT,
+    PRIMARY KEY (Order_Publisher_Id, Book_Id),
+    FOREIGN KEY (Order_Publisher_Id) REFERENCES ORDER_PUBLISHER(Order_Publisher_Id),
+    FOREIGN KEY (Book_Id) REFERENCES BOOK(Book_Id)
 );
+
+
+
+
+
+
+
+
+
